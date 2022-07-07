e82cf4a1baaa6b556ccc6778c7f059c106e36982
Pablo S. Peixoto <pablonegociosnet@gmail.com>
2022-07-07 19:49:43 -0300

    corrigido erro de lint

--- src/App.js
+++ src/App.js
@@ -58,7 +58,6 @@ class App extends React.Component {
       categories,
     } = this.state;
 
-
     return (
       <div>
         <BrowserRouter>
@@ -69,7 +68,6 @@ class App extends React.Component {
                 <Home
                   onInputChange={ this.onInputChange }
                   searchInput={ searchInput }
-
                   onClick={ this.fetchListProducts }
                   productsByTerms={ productsByTerms }
                   loading={ loading }
